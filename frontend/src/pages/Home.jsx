662caf3fa11f56480e29d2aaf16094674b01d414
Alexis-NM <alexis.na.me@gmail.com>
2023-10-31 11:42:41 +0100

    routing creation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>My Home Page</h1>;
+  return (
+    <nav>
+      <h1>My Home Page</h1>
+      <p>
+        <Link to="/movies">Movies</Link>
+      </p>
+      <p>
+        <Link to="/series">Series</Link>
+      </p>
+      <p>
+        <Link to="/favorites">Favorites</Link>
+      </p>
+    </nav>
+  );
 }
 
 export default Home;
