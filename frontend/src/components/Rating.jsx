dc6247e69c9ce1780ef4e5668735c297f800ee6f
Alexis-NM <alexis.na.me@gmail.com>
2023-11-02 20:02:23 +0100

    adding cards and categories

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -5,15 +5,15 @@ function Rating() {
     <figure>
       <div className="rating">
         <input type="radio" id="star5" name="rating" value="5" />
-        <label htmlFor="star5"> </label>
+        <label htmlFor="star5">‎</label>
         <input type="radio" id="star4" name="rating" value="4" />
-        <label htmlFor="star4"> </label>
+        <label htmlFor="star4">‎</label>
         <input type="radio" id="star3" name="rating" value="3" />
-        <label htmlFor="star3"> </label>
+        <label htmlFor="star3">‎</label>
         <input type="radio" id="star2" name="rating" value="2" />
-        <label htmlFor="star2"> </label>
+        <label htmlFor="star2">‎</label>
         <input type="radio" id="star1" name="rating" value="1" />
-        <label htmlFor="star1"> </label>
+        <label htmlFor="star1">‎</label>
       </div>
     </figure>
   );
