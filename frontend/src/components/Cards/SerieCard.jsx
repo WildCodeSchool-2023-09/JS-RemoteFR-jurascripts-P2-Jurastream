86cd56f3c40830b7d21326d0ffd287a3a5052cdf
Benoît Vandanjon <benoit@vandanjon.fr>
2023-11-10 10:24:54 +0100

    replace env string call for apikey

--- frontend/src/components/Cards/SerieCard.jsx
+++ frontend/src/components/Cards/SerieCard.jsx
@@ -5,8 +5,7 @@ import "./MediaCard.scss";
 import InfoCard from "../InfoCard";
 import { FavoritesContext } from "../FavoritesContext";
 
-
-const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const apiKey = import.meta.env.TMDB_API_KEY;
 
 function SerieCard({ serieId }) {
   const [posterPath, setPosterPath] = useState("");
