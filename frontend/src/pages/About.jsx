3f50783afa17a9f970edb2e0c010730b9cf12f44
Benoît Vandanjon <benoit@vandanjon.fr>
2023-11-10 10:43:16 +0100

    complete routes with basic about component

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+function About() {
+  return (
+    <div>
+      <h1>My About Page</h1>
+      <div>
+        <NavBar />
+      </div>
+    </div>
+  );
+}
+
+export default About;
