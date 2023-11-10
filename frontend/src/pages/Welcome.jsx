ab2bb07dc7b5473ef78933abbd526f3fe6bbab8d
Alexis-NM <127667742+Alexis-NM@users.noreply.github.com>
2023-11-10 11:11:32 +0100

    Update Welcome.jsx

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,11 +1,22 @@
-import NavBarWelcome from "../components/NavBarWelcome";
+import NavBar from "../components/NavBar";
+import "./Welcome.scss";
+import "../components/NavBar.scss";
 
 function Welcome() {
   return (
-    <div>
-      <NavBarWelcome />
-      <h1>Welcome</h1>
-    </div>
+    <body className="welcome">
+      <div>
+        <nav>
+          <NavBar />
+        </nav>
+        <h2>
+          <img src="./src/assets/LOGO.png" alt="logo" />
+        </h2>
+        <form>
+          <input name="uncontrolled" placeholder="Subscribe" />
+        </form>
+      </div>
+    </body>
   );
 }
 
